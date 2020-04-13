22c06e2df3d71d4929e77dcedeae659a077f0999
rod <rodnascimentolopes@gmail.com>
2020-04-13 19:46:45 -0300

    responsive layout 468px and 320px

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,21 @@ $(document).ready(function() {
     nav.toggleClass('responsive');
     });
 });
+
+$("#dot-1").click(function() {
+    $("#dot-1").addClass("active")
+    $("#dot-2").removeClass("active")
+    $("#dot-3").removeClass("active")
+});
+
+$("#dot-2").click(function() {
+    $("#dot-2").addClass("active")
+    $("#dot-1").removeClass("active")
+    $("#dot-3").removeClass("active")
+});
+
+$("#dot-3").click(function() {
+    $("#dot-3").addClass("active")
+    $("#dot-1").removeClass("active")
+    $("#dot-2").removeClass("active")
+});
